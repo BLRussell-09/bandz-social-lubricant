25c13df7dbb83804adf60821079bdfeef5676033
Corey Lutrick <coreylutrick@gmail.com>
2018-03-13 20:31:58 -0700

    Added album data(album name, song names, song durations)

---
+++ main.js
@@ -0,0 +1,28 @@
+var Albums = 
+[
+  {
+    albumName: "Slip n Slide",
+    songList: ["Water or Silicon?", "It Puts The Lube On It's Skin", "Lubed Up And Let Down", "Emergency Lube", "Paula Dean's Butter Stash"],
+    songDuration: ["2:34", "3:45", "4:56", "5:67", "3:56"]
+  },
+  {
+    albumName: "Living the Lube Life",
+    songList: ["Lube 24/7", "Dinosaur Friendly", "Not For Consumption", "Mr. Rogers Secret", "Rehydrated Raisins"],
+    songDuration: ["2:50", "3:35", "4:10", "5:57", "3:56"]
+  },
+  {
+    albumName: "Slip n Slide the II",
+    songList: ["Gimme More", "Your Grandma Is My Best Friend", "Smells Like Lube", "Lube Is Love, Lube is Life", "Lube Cellar"],
+    songDuration: ["2:37", "3:33", "4:16", "5:11", "3:40"]
+  },
+  {
+    albumName: "Confessions of a Lube Bottle",
+    songList: ["The Things I've Seen", "The Things I've Done", "The Places I've Been", "Keep Refrigerated", "Not Jello"],
+    songDuration: ["7:54", "5:42", "1:23", "6:46", "3:34"]
+  },
+  {
+    albumName: "The Final Bottle",
+    songList: ["Fond Memories", "Lube Legacy", "Socially Awkward", "Puddle Of Lube", "Last Drop Of the Bottle"],
+    songDuration: ["2:47", "3:38", "4:29", "5:18", "3:37"]
+  }
+];
